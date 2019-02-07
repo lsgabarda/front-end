49b2c22b21cff62371b818023de2b962ae09da92
Gelline Gruta <gelline.gruta@shoresuite.com>
2019-02-07 16:43:52 +0800

    Made a dropdown with buttons as selection

--- src/App.js
+++ src/App.js
@@ -2,24 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Dropdown from './components/Dropdown'
+
 class App extends Component {
   render() {
+    const anArray = ["Value1","Value2","Value3","Value4","Value5",];
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossOrigin="anonymous" />
+        <Dropdown options={anArray}/>
       </div>
     );
   }
